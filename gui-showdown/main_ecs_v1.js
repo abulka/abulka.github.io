9eb26ab84c6c2f6d81127ff0f7650354b1c3242f
Andy Bulka <abulka@gmail.com>
2019-07-09 22:37:11 +1000

    added gui-showdown subdir

---
+++ gui-showdown/main_ecs_v1.js
@@ -0,0 +1,215 @@
+//
+// Model
+//
+
+var model = {
+  welcomemsg: "Welcome", 
+  user: {
+    firstname: "Sam", 
+    surname: "Smith"
+  }
+}
+
+const world = new Ecs();
+
+//
+// Components
+//
+
+class ModelRef extends NestedDictAccess {  // Reference to a shared model object
+  constructor(model, keys) {
+    super(model, keys)
+    this.finalstr = "";
+  }
+}
+
+class MultiModelRef {  // Refers to multiple model fields, since can only have one component per entity can't have multiple ModelRefs
+  constructor(refs) {
+    this.refs = refs;  // list of ModelRef
+  }
+}
+class GuiControlRef {  // Mediator (entity + this component) needs to know about a wxPython gui control
+  constructor(ref) {
+    this.ref = ref
+  }
+}
+
+class Flag {}  // Mediator (entity + this component) might have a flag to indicate some behaviour is wanted
+
+//
+// Wire up and build everything
+//
+
+const entity_welcome_left = world.entity('entity_welcome_left')
+entity_welcome_left.setComponent('c_model_ref', new ModelRef(model, ['welcomemsg']))
+entity_welcome_left.setComponent('c_gui_div', new GuiControlRef('welcome'))  // id of div to hold welcome message, top left
+
+const entity_welcome_user_right = world.entity('entity_welcome_user_right')
+entity_welcome_user_right.setComponent('c_multi_model_ref', new MultiModelRef(
+  [
+    new ModelRef(model, ['welcomemsg']),
+    new ModelRef(model, ["user", "firstname"]),
+    new ModelRef(model, ["user", "surname"]),
+  ]
+));
+entity_welcome_user_right.setComponent('c_gui_div', new GuiControlRef('welcome-user'));  // id of div to hold welcome + user message, top right
+
+const entity_edit_welcome_msg = world.entity('entity_edit_welcome_msg')
+entity_edit_welcome_msg.setComponent('c_model_ref', new ModelRef(model, ['welcomemsg']));
+entity_edit_welcome_msg.setComponent('c_gui_input', new GuiControlRef('welcome'));  // name (not id) of input to hold welcome message
+
+const entity_edit_user_name_msg = world.entity('entity_edit_user_name_msg')
+entity_edit_user_name_msg.setComponent('c_model_ref', new ModelRef(model, ["user", "firstname"]));
+entity_edit_user_name_msg.setComponent('c_gui_input', new GuiControlRef('firstname'));  // name (not id) of input to hold first name
+
+const entity_edit_user_surname_msg = world.entity('entity_edit_user_surname_msg')
+entity_edit_user_surname_msg.setComponent('c_model_ref', new ModelRef(model, ["user", "surname"]));
+entity_edit_user_surname_msg.setComponent('c_gui_input', new GuiControlRef('surname'));  // name (not id) of input to hold first name
+
+const entity_dump_models = world.entity('entity_dump_models')
+entity_dump_models.setComponent('c_debug_dump_options', {verbose: false});  // dict as component is ok
+
+// Extract systems - pull info from model into component 'finalstr' field for later manipulation by other systems
+
+world.system('extract-model-ref-system', ['c_model_ref'], (entity, {c_model_ref}) => {
+  // Tip - the variables receiving the component must be named exactly the same as the component name
+  let c = c_model_ref
+  c.finalstr = c.val
+});
+world.system('extract-multi-model-ref-system', ['c_multi_model_ref'], (entity, {c_multi_model_ref}) => {
+  for (const c of c_multi_model_ref.refs) {  // each 'c' is a ModelRef component 
+    c.finalstr = c.val
+  }
+});
+
+// Case transform systems
+
+world.system('case-transform-uppercase-welcome', ['c_model_ref', 'c_uppercase_welcome'], (entity, {c_model_ref, c_uppercase_welcome}) => {
+  let c = c_model_ref
+  if (c.keys.includes("welcomemsg"))
+    c.finalstr = c.finalstr.toUpperCase()
+});
+world.system('case-transform-uppercase_welcome_user_welcome', ['c_multi_model_ref', 'c_uppercase_welcome'], (entity, {c_multi_model_ref, c_uppercase_welcome}) => {
+  for (const c of c_multi_model_ref.refs)  // each 'c' is a ModelRef component 
+    if (c.keys.includes("welcomemsg"))
+      c.finalstr = c.finalstr.toUpperCase()
+});
+world.system('case-transform-uppercase_welcome_user_user', ['c_multi_model_ref', 'c_uppercase_user'], (entity, {c_multi_model_ref, c_uppercase_user}) => {
+  for (const c of c_multi_model_ref.refs)  // each 'c' is a ModelRef component 
+    if (c.keys.includes("firstname") || c.keys.includes("surname"))
+      c.finalstr = c.finalstr.toUpperCase()
+});
+world.system('case-transform-uppercase_welcome_user', ['c_multi_model_ref', 'c_uppercase_welcome_user'], (entity, {c_multi_model_ref, c_uppercase_welcome_user}) => {
+  for (const c of c_multi_model_ref.refs)  // each 'c' is a ModelRef component
+    c.finalstr = c.finalstr.toUpperCase()
+});
+
+// Render Systems
+
+world.system('render-system-top-left', ['c_model_ref', 'c_gui_div'], (entity, {c_model_ref, c_gui_div}) => {
+  if (c_model_ref.keys.includes("welcomemsg"))
+    $('#' + c_gui_div.ref).html(c_model_ref.finalstr)
+});
+
+let msg = {}  // can't target how model ref components get found, so build up what we need here
+world.system('render-system-top-right', ['c_multi_model_ref', 'c_gui_div'], (entity, {c_multi_model_ref, c_gui_div}) => {
+  for (const c_model_ref of c_multi_model_ref.refs)
+    msg[c_model_ref.keys.slice(-1)] = c_model_ref.finalstr
+  $('#' + c_gui_div.ref).html(`${msg['welcomemsg']} ${msg['firstname']} ${msg['surname']}`)
+});
+
+world.system('render-system-text-inputs', ['c_model_ref', 'c_gui_input'], (entity, {c_model_ref, c_gui_input}) => {
+  $(`input[name=${c_gui_input.ref}]`).val(c_model_ref.finalstr)
+});
+
+world.system('render-system-dump-models', ['c_debug_dump_options'], (entity, {c_debug_dump_options}) => {
+  let part1_html = syntaxHighlight(JSON.stringify({model: model}, null, 2))
+  let part2_html = dump_world(world, c_debug_dump_options.verbose)
+  $('#debug_info').html(part1_html + '<br>' + part2_html)
+});
+
+// Util
+
+function isUpperCaseAt(str, n) {
+  return str[n]=== str[n].toUpperCase();
+}
+
+//
+// GUI events
+//
+
+$('#change_welcome_model').on('click', function(e) {
+  model.welcomemsg = isUpperCaseAt(model.welcomemsg, 1) ? model.welcomemsg.toLowerCase() : model.welcomemsg.toUpperCase()
+  world.tick()
+})
+
+$('#change_user_model').on('click', function(e) {
+  model.user.firstname = isUpperCaseAt(model.user.firstname, 1) ? model.user.firstname.toLowerCase() : model.user.firstname.toUpperCase()
+  model.user.surname = isUpperCaseAt(model.user.surname, 1) ? model.user.surname.toLowerCase() : model.user.surname.toUpperCase()
+  world.tick()
+})
+
+$('#reset_welcome_model').on('click', function(e) {
+  model.welcomemsg = "Hello"
+  world.tick()
+})
+
+$('#reset_user_model').on('click', function(e) {
+  model.user.firstname = "Fred"
+  model.user.surname = "Flinstone"
+  world.tick()
+})
+
+$("input[name=uppercase_welcome]").change(function(e) {
+  add_or_remove_component(world, 
+    $(e.target).prop('checked'), 
+    'c_uppercase_welcome', 
+    Flag, 
+    [entity_welcome_left, entity_welcome_user_right])
+  world.tick()
+})
+
+$("input[name=uppercase_user]").change(function(e) {
+  add_or_remove_component(world, 
+    $(e.target).prop('checked'), 
+    'c_uppercase_user', 
+    Flag, 
+    [entity_welcome_user_right])  
+  world.tick()
+})
+
+$("input[name=uppercase_welcome_user]").change(function(e) {
+  add_or_remove_component(world, 
+    $(e.target).prop('checked'), 
+    'c_uppercase_welcome_user', 
+    Flag, 
+    [entity_welcome_user_right])
+  world.tick()
+});
+
+$("input[name=verbose_debug]").change(function(e) {
+  let component = {verbose: $(e.target).prop('checked')}
+  entity_dump_models.setComponent('c_debug_dump_options', component)  // replaces any existing component
+  world.tick()
+});
+
+$("input[name=welcome]").keypress(function(e) {
+    model["welcomemsg"] = $(e.target).val()
+    world.tick()
+})
+
+$("input[name=firstname]").keypress(function(e) {
+  model["user"]["firstname"] = $(e.target).val()
+  world.tick()
+})
+
+$("input[name=surname]").keypress(function(e) {
+  model["user"]["surname"] = $(e.target).val()
+  world.tick()
+})
+
+$('#render-now').on('click', function(e) {
+  world.tick()
+})
+
+world.tick()
---
+++ gui-showdown/main_oo.js
@@ -0,0 +1,279 @@
+//
+// Model - The Welcome model and User model are Observable.
+//
+
+class Welcome extends Subject {
+  constructor(message) {
+      super();
+      this._msg = message == undefined ? "Welcome" : message;
+    }
+  
+    get message() {
+      return this._msg
+    }
+  
+    set message(v) {
+      this._msg = v;
+      this.notifyall(`modified ${this._msg}`)
+    }
+}
+
+class User extends Subject {
+  constructor() {
+      super();
+      this._firstname = "Sam"
+      this._surname = "Smith"
+    }
+  
+    get firstname() {
+      return this._firstname
+    }
+  
+    set firstname(v) {
+      this._firstname = v;
+      this.notifyall(`modified ${this._firstname}`)
+    }
+  
+    get surname() {
+      return this._surname
+    }
+  
+    set surname(v) {
+      this._surname = v;
+      this.notifyall(`modified ${this._surname}`)
+    }
+}
+
+class Model {  // aggregates all the sub models into one housing
+  constructor(welcome_model, user_model) {
+      this.welcome = welcome_model
+      this.user = user_model
+    }
+  
+    dirty_startup() {
+      this.dirty_all()
+      mediator_page_title.notify(null, "dirty startup")  // notify 'mediator_page_title' directly since this only happens once and 'mediator_page_title' doesn't subscribe to anyone
+    }
+
+    dirty_all() {
+      this.welcome.notifyall("dirty all")
+      this.user.notifyall("dirty all")
+    }
+}
+
+//
+// Mediators - are implemented as Observer classes, contain the display update behaviour plus refs to model and gui
+//
+
+class MediatorWelcomeLeft extends Observer {
+  constructor(welcome_model, id) {
+    super()
+    this.welcome = welcome_model  // ref to Welcome model
+    this.gui_div = id             // ref to DOM div where we want the welcome message to appear
+    this.uppercase_welcome = false
+  }
+  
+  notify(target, data) {
+    super.notify(target, data)
+    let msg = this.uppercase_welcome ? this.welcome.message.toUpperCase() : this.welcome.message
+    $('#' + this.gui_div).html(msg)
+  }
+}
+
+class MediatorWelcomeUserRight extends Observer {
+  constructor(welcome_model, user_model, id) {
+    super()
+    this.welcome = welcome_model
+    this.user = user_model
+    this.gui_div = id
+    this.uppercase_welcome = false
+    this.uppercase_user = false
+  }
+  
+  notify(target, data) {
+    super.notify(target, data)
+    let welcome = this.uppercase_welcome ? this.welcome.message.toUpperCase() : this.welcome.message
+    let firstname = this.uppercase_user ? this.user.firstname.toUpperCase() : this.user.firstname
+    let surname = this.uppercase_user ? this.user.surname.toUpperCase() : this.user.surname
+    $('#' + this.gui_div).html(welcome + ' ' + firstname + ' ' + surname )
+  }
+}
+
+class MediatorEditWelcome extends Observer {
+  constructor(welcome_model, id) {
+    super()
+    this.welcome = welcome_model
+    this.gui_input = id  // name (not id) of input to hold welcome message
+  }
+  
+  notify(target, data) {
+    super.notify(target, data)
+    assert(target == this.welcome)
+    $(`input[name=${this.gui_input}]`).val(this.welcome.message)
+  }
+}
+
+class MediatorEditUserFirstName extends Observer {
+  constructor(user_model, id) {
+    super()
+    this.user = user_model
+    this.gui_input = id
+  }
+  
+  notify(target, data) {
+    super.notify(target, data)
+    assert(target == this.user)
+    $(`input[name=${this.gui_input}]`).val(this.user.firstname)
+  }
+}
+
+class MediatorEditUserSurName extends Observer {
+  constructor(user_model, id) {
+    super()
+    this.user = user_model
+    this.gui_input = id
+  }
+  
+  notify(target, data) {
+    super.notify(target, data)
+    assert(target == this.user)
+    $(`input[name=${this.gui_input}]`).val(this.user.surname)
+  }
+}
+
+class MediatorPageTitle extends Observer {
+  constructor(s, $id) {
+    super()
+    this.s = s
+    this.$id = $id
+  }
+  
+  notify(target, data) {
+    super.notify(target, data)
+    this.$id.html(this.s)
+  }
+}
+
+class MediatorDumpModels extends Observer {
+  constructor(id) {
+    super()
+    this.gui_pre_id = id
+  }
+  
+  notify(target, data) {
+    super.notify(target, data)
+    let info = {
+      model: model,
+      mediator_welcome_left: mediator_welcome_left,
+      mediator_welcome_user_right : mediator_welcome_user_right,
+      mediator_edit_welcome_msg : mediator_edit_welcome_msg,
+      mediator_edit_user_name_msg : mediator_edit_user_name_msg,
+      mediator_edit_user_surname_msg : mediator_edit_user_surname_msg,
+    }
+    $(`#${this.gui_pre_id}`).html(syntaxHighlight(JSON.stringify(info, function(key, value) {
+
+        // skip observers or circular references will break the json
+        if (key == 'observers') { 
+          return value.id;
+        } else {
+          return value;
+        }
+
+    }, 2)))
+
+  }
+}
+
+// Util
+
+function isUpperCaseAt(str, n) {
+    return str[n]=== str[n].toUpperCase();
+}  
+
+//
+// GUI events
+//
+
+$('#change_welcome_model').on('click', function(e) {
+  model.welcome.message = isUpperCaseAt(model.welcome.message, 1) ? model.welcome.message.toLowerCase() : model.welcome.message.toUpperCase()
+})
+
+$('#change_user_model').on('click', function(e) {
+  model.user.firstname = isUpperCaseAt(model.user.firstname, 1) ? model.user.firstname.toLowerCase() : model.user.firstname.toUpperCase()
+  model.user.surname = isUpperCaseAt(model.user.surname, 1) ? model.user.surname.toLowerCase() : model.user.surname.toUpperCase()
+})
+
+$('#reset_welcome_model').on('click', function(e) {
+  model.welcome.message = "Hello"
+})
+
+$('#reset_user_model').on('click', function(e) {
+  model.user.firstname = "Fred"
+  model.user.surname = "Flinstone"
+})
+
+$("input[name=uppercase_welcome]").change(function(e) {
+  mediator_welcome_left.uppercase_welcome = $(e.target).prop('checked')
+  mediator_welcome_user_right.uppercase_welcome = $(e.target).prop('checked')
+
+  // Notify affected mediators directly, rather than inefficient model.dirty_all()
+  mediator_welcome_left.notify(null, "display option change")
+  mediator_welcome_user_right.notify(null, "display option change")
+  mediator_dump_models.notify(null, "display option change")
+})
+
+$("input[name=uppercase_user]").change(function(e) {
+  mediator_welcome_user_right.uppercase_user = $(e.target).prop('checked')
+
+  mediator_welcome_user_right.notify(null, "display option change")
+  mediator_dump_models.notify(null, "display option change")
+})
+
+$("input[name=uppercase_welcome_user]").change(function(e){
+  mediator_welcome_user_right.uppercase_welcome = $(e.target).prop('checked')
+  mediator_welcome_user_right.uppercase_user = $(e.target).prop('checked')
+
+  mediator_welcome_user_right.notify(null, "display option change")
+  mediator_dump_models.notify(null, "display option change")
+});
+
+$( "input[name=welcome]" ).keypress(function(e) {  // use 'change' if you want to wait for ENTER
+  model.welcome.message = $(e.target).val()
+});
+
+$("input[name=firstname]").keypress(function(e) {
+  model.user.firstname = $(e.target).val()
+})
+
+$("input[name=surname]").keypress(function(e) {
+  model.user.surname = $(e.target).val()
+})
+
+$('#render-now').on('click', function(e) {
+  model.dirty_all()
+})
+
+//
+// Wire up and build everything
+//
+
+model = new Model(new Welcome(), new User())
+mediator_welcome_left = new MediatorWelcomeLeft(model.welcome, "welcome")
+mediator_welcome_user_right = new MediatorWelcomeUserRight(model.welcome, model.user, 'welcome-user')
+mediator_edit_welcome_msg = new MediatorEditWelcome(model.welcome, 'welcome')
+mediator_edit_user_name_msg = new MediatorEditUserFirstName(model.user, 'firstname')
+mediator_edit_user_surname_msg = new MediatorEditUserSurName(model.user, 'surname')
+mediator_dump_models = new MediatorDumpModels("debug_info")
+mediator_page_title = new MediatorPageTitle("Gui wired via OO + Observer", $('#title > h1'))
+
+// Observer Wiring
+model.welcome.add_observer(mediator_welcome_left)
+model.welcome.add_observer(mediator_welcome_user_right)
+model.welcome.add_observer(mediator_edit_welcome_msg)
+model.user.add_observer(mediator_welcome_user_right)
+model.user.add_observer(mediator_edit_user_name_msg)
+model.user.add_observer(mediator_edit_user_surname_msg)
+model.welcome.add_observer(mediator_dump_models)  // debug mediator is also an observer of the models
+model.user.add_observer(mediator_dump_models)
+
+model.dirty_startup()  // initialise the gui with initial model values
